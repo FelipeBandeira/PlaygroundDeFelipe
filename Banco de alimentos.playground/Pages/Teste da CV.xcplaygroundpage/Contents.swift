ae20333e1837e496fe676c97daaaa92606df4c90
Felipe Bandeira Ramos <felipebandeira@br-ufpe-07.local>
2020-04-14 12:09:22 -0300

    Conseguimos fazer a collection view funcionar! Ainda faltam as bordas arredondadas, mas isso é o de menos. Agora, o próximo passo é configurar o toque para levar até a última tela, e configurar eessa tela, que é o pop-up

--- Banco de alimentos.playground/Pages/Teste da CV.xcplaygroundpage/Contents.swift
+++ Banco de alimentos.playground/Pages/Teste da CV.xcplaygroundpage/Contents.swift
@@ -8,7 +8,7 @@ import PlaygroundSupport
 class CelulaDosVegetais: UICollectionViewCell{
     
     public let ilustracao: UIImageView  = UIImageView(frame: CGRect(x: 0, y: 0, width: 337.7, height: 91.67))
-    public let titulo: UILabel = UILabel(frame: CGRect(x: 59.5, y: -19, width: 214.61, height: 55.43))
+    public let titulo: UILabel = UILabel(frame: CGRect(x: 59.5, y: 19, width: 214.61, height: 55.43))
     
     // 3) Não entendi esse init. Por que override? Por que super? Onde estão os parâmetros do init?
     public override init(frame: CGRect){
@@ -17,6 +17,7 @@ class CelulaDosVegetais: UICollectionViewCell{
         titulo.font = UIFont(name: "PTSans-Bold", size: 40)
         titulo.textColor = .white
         self.addSubview(titulo)
+        
     }
     
     
@@ -37,27 +38,29 @@ class EstruturaDosVegetais {
 
 
 class MyViewController4 : UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-   
     
-    let listaDeVegetais = [EstruturaDosVegetais(nome: "Coentro", foto: UIImage(contentsOfFile: "coentro-5.jpg")), EstruturaDosVegetais(nome: "Beringela", foto: UIImage(contentsOfFile: "sh_beringela_432696973.jpg")), EstruturaDosVegetais(nome: "Cebola Roxa", foto: UIImage(contentsOfFile: "cebola-roxa-768x307-b88dde90.jpg")), EstruturaDosVegetais(nome: "Cenoura", foto: UIImage(contentsOfFile: "beneficios-da-cenoura_14186_l.jpg"))]
+    
+    let listaDeVegetais = [EstruturaDosVegetais(nome: "Coentro", foto: UIImage(named: "coentro-5.jpg")), EstruturaDosVegetais(nome: "Beringela", foto: UIImage(named: "sh_beringela_432696973.jpg")), EstruturaDosVegetais(nome: "Cebola Roxa", foto: UIImage(named: "cebola-roxa-768x307-b88dde90.jpg")), EstruturaDosVegetais(nome: "Cenoura", foto: UIImage(named: "beneficios-da-cenoura_14186_l.jpg"))]
     
     
     override func viewDidLoad() {
         
+        view.backgroundColor = .white
+        
         let meuLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        meuLayout.sectionInset = UIEdgeInsets(top: 86, left: 552, bottom: 372, right: 1193)
+        meuLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         meuLayout.itemSize = CGSize(width: 337.7, height: 91.67)
+        meuLayout.minimumLineSpacing = 50
         
         
         let minhaCollectionView = UICollectionView(frame: CGRect(x: 86, y: 552, width: 1193, height: 372), collectionViewLayout: meuLayout)
         
         minhaCollectionView.register(CelulaDosVegetais.self, forCellWithReuseIdentifier: "id")
-        minhaCollectionView.backgroundColor = .white
+        minhaCollectionView.backgroundColor = .green
         minhaCollectionView.dataSource = self
         
         view.addSubview(minhaCollectionView)
         
-        self.view = view
     }
     
     
@@ -68,8 +71,8 @@ class MyViewController4 : UIViewController, UICollectionViewDelegate, UICollecti
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let minhaCelula = collectionView.dequeueReusableCell(withReuseIdentifier: "id", for: indexPath) as? CelulaDosVegetais
         
-        minhaCelula?.ilustracao.image = listaDeVegetais[indexPath.section].fotoDoVegetal
-        minhaCelula?.titulo.text = listaDeVegetais[indexPath.section].nomeDoVegetal
+        minhaCelula?.ilustracao.image = listaDeVegetais[indexPath.item].fotoDoVegetal
+        minhaCelula?.titulo.text = listaDeVegetais[indexPath.item].nomeDoVegetal
         return minhaCelula!
     }
     
@@ -78,4 +81,4 @@ class MyViewController4 : UIViewController, UICollectionViewDelegate, UICollecti
 
 let mvc4 = MyViewController4(screenType: .ipadPro12_9, isPortrait: false)
 //mvc.preferredContentSize = CGSize(width: 768, height: 1024)
-PlaygroundPage.current.liveView = mvc4.scale(to: 0.3)
+PlaygroundPage.current.liveView = mvc4.scale(to: 0.5)
